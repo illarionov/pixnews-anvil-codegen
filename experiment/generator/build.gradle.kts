6db82349ef7468a18345a5f44456de16fcc10b02
Alexey Illarionov <alexey@0xdc.ru>
2024-01-13 15:43:24 +0300

    Move configuration of generator modules to precompiled script plugin (#3)

--- experiment/generator/build.gradle.kts
+++ experiment/generator/build.gradle.kts
@@ -5,27 +5,8 @@
  */
 
 plugins {
-    id("ru.pixnews.anvil.codegen.build-logic.project.kotlin.library")
-    id("ru.pixnews.anvil.codegen.build-logic.project.test")
-    id("ru.pixnews.anvil.codegen.build-logic.project.publish")
-    kotlin("kapt")
+    id("ru.pixnews.anvil.codegen.build-logic.project.generator")
 }
 
 group = "ru.pixnews.anvil.codegen.experiment.generator"
 version = "0.1-SNAPSHOT"
-
-dependencies {
-    api(libs.anvil.compiler.api)
-    implementation(libs.anvil.compiler.utils)
-    implementation(libs.kotlinpoet) { exclude(module = "kotlin-reflect") }
-    implementation(projects.common)
-
-    compileOnly(libs.auto.service.annotations)
-    kapt(libs.auto.service.compiler)
-
-    testImplementation(libs.anvil.annotations.optional)
-    testImplementation(libs.assertk)
-    testImplementation(libs.dagger)
-    testImplementation(projects.testUtils)
-    testImplementation(testFixtures(libs.anvil.compiler.utils))
-}
